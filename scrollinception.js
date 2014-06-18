014aca3c80c108e4ee26114d6553870f2e28bd9b
Shinze <gaetan.ark@gmail.com>
2014-06-18 15:11:44 +0200

    Js hinted

--- scrollinception.js
+++ scrollinception.js
@@ -1,18 +1,20 @@
-$.scrollinception = function() {
+var $;
+$.scrollinception = function () {
+    'use strict';
     var container = '<div class="scrollinception-container" />',
         scrollbutton = '<a href="#" class="scrollinception-button"><span class="scrollinception-icon">⇵</span></a>';
 
     $('[data-scrollinception]').wrap(container);
     $('.scrollinception-container').append(scrollbutton);
 
-    $('.scrollinception-button').on('click', function(event) {
-      event.preventDefault();
-      var $btn = $(this);
-      $(this).hide();
-      $(window).on('scroll', function() {
-          $btn.show();
-          $(this).unbind('scroll');
-      });
+    $('.scrollinception-button').on('click', function (event) {
+        event.preventDefault();
+        var $btn = $(this);
+        $(this).hide();
+        $(window).on('scroll', function () {
+            $btn.show();
+            $(this).unbind('scroll');
+        });
     });
 };
 
